c3c0e1c3ff4b6a981e347172c6e17784d6c97c7e
Tsuji Tomonori <160441107@ccalumni.meijo-u.ac.jp>
2019-07-18 19:10:33 +0900

    update 正答率, 平均スコアを算出
    モデル情報の追加
    正答率, 平均スコアの算出

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 # server.py
 from flask import Flask, render_template, request
+import glob
+import os
+import csv
 
 app = Flask(__name__)
 
-model_name = "model1"
-heads = ['odai', 'pre', 'axe', 'airplane', 'apple', 'banana',
-         'bicycle', 'car', 'donut', 'fish', 'hat', 'house']
-value_list = [["banana", "banana", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]]
+model_info = {}
+labels = {}
 
 
 @app.route("/")
@@ -14,20 +15,72 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/log", methods=["POST", "GET"])
-def log_model():
+@app.route("/log/<model_name>", methods=["POST", "GET"])
+def log_model(model_name):
+    global model_info
     if request.method == "POST":
         value = []
-        for key in heads:
+        for key in model_info[model_name]["heads"]:
             res = request.form[key]
             value.append(res)
-        global value_list
-        value_list.append(value)
-        return render_template("model.html", model_name=model_name, heads=heads, value_list=value_list)
-    else:
-        return render_template("model.html", model_name=model_name, heads=heads, value_list=value_list)
+        model_info[model_name]["value_list"].append(value)
+    (acc, ave) = cal(model_name)
+    return render_template("model.html",
+                           model_name=model_name,
+                           heads=model_info[model_name]["heads"],
+                           value_list=model_info[model_name]["value_list"],
+                           acc=acc,
+                           ave=ave)
+
+
+def init():
+    """ 初期化する関数.
+
+    model フォルダーを読み込み, model_info の設定をする
+    """
+    path = "./model"
+    files = os.listdir(path)
+    # モデル名を フォルダー名から取得
+    files_dir = [f for f in files if os.path.isdir(os.path.join(path, f))]
+    global model_info, labels
+    for model in files_dir:
+        model_info[model] = {}
+        # label.csv を開き ラベルを取得する
+        with open(path + "/" + model + "/label.csv", "r") as f:
+            reader = csv.DictReader(f)
+            l = [row for row in reader]
+        # ラベルを labels にセットする
+        labels[model] = {key: int(value) + 2 for key, value in l[0].items()}
+        # model_info の設定
+        head = ["odai", "pre"]
+        model_info[model]["heads"] = head + [key for key in l[0]]
+        model_info[model]["value_list"] = []
+
+
+def cal(model):
+    """ 正答率, 平均スコアを算出する関数.
+
+    Args:
+        model (str): モデル名
+
+    Return:
+        int: 正答率
+        int: 平均スコア
+
+    """
+    global model_info, labels
+    acc_ave = 0
+    acc_count = 0
+    for score in model_info[model]["value_list"]:
+        if score[0] == score[1]:
+            acc_ave += int(score[labels[model][score[0]]])
+            acc_count += 1
+    if acc_count == 0:
+        return (0, 0)
+    return (acc_count/len(model_info[model]["value_list"]), acc_ave/acc_count)
 
 
 if __name__ == "__main__":
+    init()
     app.debug = True
     app.run(host="0.0.0.0", port=8888)
